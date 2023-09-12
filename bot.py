5aa8734ce995d2abed616a477ef60ef28bc67313
Lilproskater <sultanovmirodil@mail.ru>
2023-09-12 12:41:22 +0500

    Removed comment from try-except

--- bot.py
+++ bot.py
@@ -43,33 +43,32 @@ async def get_google_weather(search='tashkent'):
     response = requests.get('https://www.google.com/search', headers=headers, params=params)
     pmc = json.loads(r_search(r'var pmc=\'{.*}\';', response.text)[0][9:-2].replace('\\\\', '\\').replace('\\x22', '"'))
     tree = html.fromstring(response.text)
-    # try:
-    location = tree.xpath("//div[@id='wob_loc']")[0].text
-    today = tree.xpath("//div[@id='wob_dts']")[0].text[:-6].capitalize()
-    forecast = tree.xpath("//span[@id='wob_dc']")[0].text.capitalize()
-    img_base64 = tree.xpath("//div[contains(@class, 'DxhUm')]//img")[0].attrib['src']
-    print(response.text)
-    day_temp = tree.xpath("//div[contains(@class, 'gNCp2e')]//span")[0].text + '°'
-    night_temp = tree.xpath("//div[contains(@class, 'ZXCv8e')]//span")[0].text + '°'
-    precipitation = tree.xpath("//span[@id='wob_pp']")[0].text
-    humidity = tree.xpath("//span[@id='wob_hm']")[0].text
-    wind_speed = tree.xpath("//span[@id='wob_ws']")[0].text
-    hourly_w = dict(zip(
-        [pmc['wobnm']['wobhl'][x]['dts'].capitalize() for x in range(0, 15, 3)],
-        [pmc['wobnm']['wobhl'][x]['tm'] + '°' for x in range(0, 15, 3)])
-    )
-    result = location + ' - ' + today + ', ' + forecast + '\n'
-    result += 'Day: ' + day_temp + '\n'
-    result += 'Night: ' + night_temp + '\n\n'
-    result += 'Precipitation: ' + precipitation + '\n'
-    result += 'Humidity: ' + humidity + '\n'
-    result += 'Wind speed: ' + wind_speed + '\n\n'
-    result += 'Hourly weather:\n'
-    for w_time, temp in hourly_w.items():
-        result += ', '.join(w_time.split(' ')) + ' - ' + temp + '\n'
-    return img_base64, result
-    # except Exception as _:
-    #     return None, None
+    try:
+        location = tree.xpath("//div[@id='wob_loc']")[0].text
+        today = tree.xpath("//div[@id='wob_dts']")[0].text[:-6].capitalize()
+        forecast = tree.xpath("//span[@id='wob_dc']")[0].text.capitalize()
+        img_base64 = tree.xpath("//div[contains(@class, 'DxhUm')]//img")[0].attrib['src']
+        day_temp = tree.xpath("//div[contains(@class, 'gNCp2e')]//span")[0].text + '°'
+        night_temp = tree.xpath("//div[contains(@class, 'ZXCv8e')]//span")[0].text + '°'
+        precipitation = tree.xpath("//span[@id='wob_pp']")[0].text
+        humidity = tree.xpath("//span[@id='wob_hm']")[0].text
+        wind_speed = tree.xpath("//span[@id='wob_ws']")[0].text
+        hourly_w = dict(zip(
+            [pmc['wobnm']['wobhl'][x]['dts'].capitalize() for x in range(0, 15, 3)],
+            [pmc['wobnm']['wobhl'][x]['tm'] + '°' for x in range(0, 15, 3)])
+        )
+        result = location + ' - ' + today + ', ' + forecast + '\n'
+        result += 'Day: ' + day_temp + '\n'
+        result += 'Night: ' + night_temp + '\n\n'
+        result += 'Precipitation: ' + precipitation + '\n'
+        result += 'Humidity: ' + humidity + '\n'
+        result += 'Wind speed: ' + wind_speed + '\n\n'
+        result += 'Hourly weather:\n'
+        for w_time, temp in hourly_w.items():
+            result += ', '.join(w_time.split(' ')) + ' - ' + temp + '\n'
+        return img_base64, result
+    except Exception as _:
+        return None, None
 
 
 @dp.message_handler(commands=['start'])
@@ -143,7 +142,7 @@ async def namaztoday(message: types.Message):
 
 
 @dp.message_handler(commands=['googleweather'])
-async def googleweather(message: types.Message):
+async def google_weather(message: types.Message):
     args = message.text.split()
     if len(args) > 2:
         await message.reply('Error in given arguments')
